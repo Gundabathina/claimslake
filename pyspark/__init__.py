c1e8170cb69bef4f8afa1f1398b5a601a43b8d19
Prem Teja <premteja01@gmail.com>
2026-07-22 09:15:01 -0500

    feat(pyspark): add pyspark package init (Milestone 3)

---
+++ pyspark/__init__.py
@@ -0,0 +1,8 @@
+"""
+ClaimsLake pyspark package.
+
+Contains the Bronze -> Silver transformation jobs built in Milestone 3.
+See pyspark/README.md for the architecture, and run the pipeline with:
+
+    python -m pyspark.src.silver_pipeline --all
+"""
