8522dace923580688d44a0743cfb65a37646dba5
Prem Teja <premteja01@gmail.com>
2026-07-23 17:06:36 -0500

    refactor(spark): rename local pyspark package to spark_jobs (add spark_jobs/__init__.py)

---
+++ spark_jobs/__init__.py
@@ -0,0 +1,8 @@
+"""
+ClaimsLake spark_jobs package.
+
+Contains the Bronze -> Silver transformation jobs built in Milestone 3.
+See spark_jobs/README.md for the architecture, and run the pipeline with:
+
+    python -m spark_jobs.src.silver_pipeline --all
+"""
