ff0858b4f12b5fa1396a5e4a106735fdbc88b267
Prem Teja <premteja01@gmail.com>
2026-07-23 17:21:28 -0500

    refactor(spark): remove old pyspark/__init__.py (moved to spark_jobs)

--- pyspark/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-ClaimsLake pyspark package.
-
-Contains the Bronze -> Silver transformation jobs built in Milestone 3.
-See pyspark/README.md for the architecture, and run the pipeline with:
-
-    python -m pyspark.src.silver_pipeline --all
-"""
