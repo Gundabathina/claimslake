4b378f13f9935585dbf17ffc00dd81945672f892
Prem Teja <premteja01@gmail.com>
2026-07-23 17:22:02 -0500

    refactor(spark): remove old pyspark/src/__init__.py (moved to spark_jobs)

--- pyspark/src/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-pyspark/src package.
-
-Bronze -> Silver transformation modules for ClaimsLake (Milestone 3):
-spark_session, schemas, readers, cleaners, deduplication, validators,
-transformations, writers, silver_pipeline. See pyspark/README.md.
-"""
