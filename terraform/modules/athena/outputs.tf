1ec2cf688f049b880938f8a17bcc5909b78c559e
Prem Teja <premteja01@gmail.com>
2026-07-30 05:57:05 -0500

    Create outputs.tf

---
+++ terraform/modules/athena/outputs.tf
@@ -0,0 +1,11 @@
+# Outputs for the Athena module.
+
+output "workgroup_name" {
+  description = "Name of the Athena workgroup."
+  value       = aws_athena_workgroup.this.name
+}
+
+output "workgroup_arn" {
+  description = "ARN of the Athena workgroup."
+  value       = aws_athena_workgroup.this.arn
+}
